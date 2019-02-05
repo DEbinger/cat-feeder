378a6e0af8e069b67b6cc966878f1f4b7e4ba8b6
Steven Cable <steven.cable12@gmail.com>
2019-02-04 21:54:10 -1000

    working on navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/button'
+import Button from './components/button';
+//import Navbar from './components/navbar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             click here to stuff my feline
@@ -21,6 +24,7 @@ class App extends Component {
             Learn React
           </a>
           <br/>
+          <Navbar/>
           <h1 className = "buttons">
           <Button/>
           </h1>
--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,11 +1,7 @@
 import React, { Component } from "react"
 //import { Component } from 'react';
 
-class Button extends React.Component {
-    // var handleIt = () => {
-    //     alert("Fuck Off Nick");
-    // }
-
+class Button extends Component {
     render () {
         return(
             <button onClick={function handleIt(){
